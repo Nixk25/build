d0b1f096407ea5b4b19d13a74921fff455898ea8
Nick <nick2003@email.cz>
2022-11-11 16:21:53 +0100

    nahrani Slidera

--- js/main.js
+++ js/main.js
@@ -11,4 +11,32 @@ $(document).ready(function(){
    close.on("click" , function(){
       modal.removeClass("modal_active");
    });
+
+   $(".slider").slick({
+      slidesToShow: 3,
+      prevArrow: $(".arrow-left"),
+      nextArrow: $(".arrow-right"),
+      autoplay: true,
+      responsive: [
+         {
+           breakpoint: 1200,
+           settings: {
+             slidesToShow: 2,
+           }
+         },
+         {
+           breakpoint: 768,
+           settings: {
+             slidesToShow:1
+           }
+         },
+         {
+           breakpoint: 480,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1
+           }
+         }
+         ]
+   });
 });
